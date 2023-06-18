a48b5fa3ff7877fa1d0920426d132e17cef4fb98
Jing Hua <tohjinghua123@gmail.com>
2023-06-17 17:51:27 -0700

    hume voice

--- backend/routers/voice.py
+++ backend/routers/voice.py
@@ -1,6 +1,32 @@
-from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
+from fastapi import APIRouter, UploadFile, HTTPException
+from fastapi.responses import FileResponse
 
-from database import get_db
+import os
+import services.hume as humes_services
 
 router = APIRouter(prefix="/voice")
+
+
+@router.post('/upload')
+async def upload_photo(file: UploadFile):
+    contents = await file.read()
+    with open(file.filename, 'wb+') as f:
+        f.write(contents)
+
+    return {"filename": file.filename}
+
+
+@router.get('/read/{filename}')
+async def get_photo(filename: str):
+    if not os.path.exists(filename):
+        raise HTTPException(status_code=400,
+                            detail="File name does not exist!")
+    return FileResponse(filename)
+
+
+@router.get('/mood/{filename}')
+async def photo_mood(filename: str):
+    if not os.path.exists(filename):
+        raise HTTPException(status_code=400,
+                            detail="File name does not exist!")
+    return humes_services.process_voice_hume(filename)
--- backend/services/hume.py
+++ backend/services/hume.py
@@ -1,7 +1,7 @@
 from config import get_settings
 
 from hume import HumeBatchClient
-from hume.models.config import FaceConfig
+from hume.models.config import FaceConfig, LanguageConfig
 
 settings = get_settings()
 
@@ -18,4 +18,20 @@ def process_image_hume(image_url: str):
     predictions = full_predictions[0]["results"]["predictions"]
     face_predictions = predictions[0]["models"]["face"]["grouped_predictions"]
     emotions = face_predictions[0]['predictions'][0]['emotions']
+    return emotions
+
+
+def process_voice_hume(voice_url: str):
+    config = LanguageConfig()
+    job = client.submit_job([
+        "https://storage.googleapis.com/hume-test-data/audio/ninth-century-laugh.mp3"
+    ], [config])
+    job.await_complete()
+
+    full_predictions = job.get_predictions()
+    predictions = full_predictions[0]["results"]["predictions"]
+    face_predictions = predictions[0]["models"]["language"][
+        "grouped_predictions"]
+    emotions = face_predictions[0]['predictions'][0]['emotions']
+
     return emotions
